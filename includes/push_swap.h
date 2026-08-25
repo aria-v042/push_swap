d4f10651cb919ccdcf63ddb8675a28c40bf1baaa
aria <redcore_one@proton.me>
2026-08-25 12:54:34 +0100

    add: push_swap.h header file

---
+++ includes/push_swap.h
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: frodrig2 <frodrig2@students.42porto.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/25 12:42:26 by frodrig2          #+#    #+#             */
+/*   Updated: 2026/08/25 12:52:02 by frodrig2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include "libft.h"
+# include <limits.h>
+
+/* ==== MAIN (srcs/main/) ==== */
+
+// TODO
+
+/* ==== UTILS (srcs/utils/) ==== */
+
+// TODO
+
+/* ==== PARSING (srcs/parse/) ==== */
+
+// TODO
+
+/* ==== OPERATIONS (srcs/ops/) ==== */
+
+// TODO
+
+/* ==== SORTING STRATEGIES (srcs/sort/) ==== */
+
+// TODO
+
+/* tiny */
+/* simple - O(n^2) */
+/* medium - O(n sqrt(n)) */
+/* complex - O(n log n) */
+
+/* ==== BENCHMARKS (srcs/bench/) ==== */
+
+// TODO
+
+#endif
