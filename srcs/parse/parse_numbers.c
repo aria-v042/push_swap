d4c5d62131d543dda8deb91f70973e9dc6103edc
zulazzzul <nicole.nr.rebelo@gmail.com>
2026-08-28 21:53:18 +0100

    fix: variable name and missing ;

--- srcs/parse/parse_numbers.c
+++ srcs/parse/parse_numbers.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parse_numbers.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: frodrig2 <frodrig2@students.42porto.com>   +#+  +:+       +#+        */
+/*   By: nd-abreu <nd-abreu@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/28 20:10:37 by frodrig2          #+#    #+#             */
-/*   Updated: 2026/08/28 21:15:18 by frodrig2         ###   ########.fr       */
+/*   Updated: 2026/08/28 21:51:28 by nd-abreu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,17 +42,17 @@ static int	parse_integer(t_data *data, char *arg)
 
 int	parse_numbers(t_data *data, char *arg)
 {
-	char	**split_arg;
+	char	**split_args;
 	int		i;
-	int		exit_code
+	int		exit_code;
 
-	split_arg = ft_split(arg, " ");
-	if (!split_arg || !split_arg[0])
+	split_args = ft_split(arg, " ");
+	if (!split_args || !split_args[0])
 		return (-1);
 	i = 0;
-	while (split_arg[i] && exit_code == 0)
+	while (split_args[i] && exit_code == 0)
 	{
-		exit_code = parse_integer(data, split_arg[i]);
+		exit_code = parse_integer(data, split_args[i]);
 		i++;
 	}
 	if (exit_code != 0)
