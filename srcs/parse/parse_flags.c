04223acd01c45faa3e439fe8f3d2f726a3ed68de
aria <redcore_one@proton.me>
2026-08-28 23:32:45 +0100

    fix: typo in header include

--- srcs/parse/parse_flags.c
+++ srcs/parse/parse_flags.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "push_swap.c"
+#include "push_swap.h"
 
 int	parse_flag(t_data *data, char *arg)
 {
