1c8f4f12c60868f3dc68a8413bfe058e1a5a5045
aria <redcore_one@proton.me>
2026-08-29 02:22:32 +0100

    add: stack utility functions

--- srcs/utils/stack_utils.c
+++ srcs/utils/stack_utils.c
@@ -12,13 +12,50 @@
 
 #include "push_swap.h"
 
-int	stack_dup(t_stack *s, int num)
+static t_stack	*stack_get_bottom(t_stack *node)
 {
-	while (s)
+	if (!node)
+		return (NULL);
+	while (node->next)
+		node = node->next;
+	return (node);
+}
+
+
+int	stack_check_dup(t_stack *node, int num)
+{
+	while (node)
 	{
-		if (s->value == num)
+		if (node->value == num)
 			return (1);
-		s = s->next;
+		node = node->next;
 	}
 	return (0);
+}
+
+t_stack	*stack_new_node(int value)
+{
+	t_stack	*new_node;
+
+	new_node = malloc(sizeof(t_stack));
+	if (!new_node)
+		return (NULL);
+	new_node->value = value;
+	new_node->next = NULL;
+	return (new_node);
+}
+
+void	stack_add_bottom(t_stack **stack_p, t_stack *new_node)
+{
+	t_stack	*bottom;
+
+	if (!stack_p || !new_node)
+		return ;
+	if (!*stack_p)
+		*stack_p = new_node;
+	else
+	{
+		bottom = stack_get_bottom(*stack_p);
+		bottom->next = new_node;
+	}
 }
