5abd864733bd6214edd61e291b43a339ac16d9b0
aria <redcore_one@proton.me>
2026-08-30 02:35:59 +0100

    fix: add operation functions' declarations to the header file

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -75,6 +75,18 @@ int		parse_numbers(t_data *data, char *arg);
 
 /* ==== OPERATIONS (srcs/ops/) ==== */
 
+void	sa(t_data *data);
+void	sb(t_data *data);
+void	ss(t_data *data);
+void	pa(t_data *data);
+void	pb(t_data *data);
+void	ra(t_data *data);
+void	rb(t_data *data);
+void	rr(t_data *data);
+void	rra(t_data *data);
+void	rrb(t_data *data);
+void	rrr(t_data *data);
+
 /* ==== SORTING STRATEGIES (srcs/sort/) ==== */
 
 void	set_disorder(t_data *data);
