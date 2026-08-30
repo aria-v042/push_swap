24864355b5d6463fdeb3733924a730bddf606099
aria <redcore_one@proton.me>
2026-08-30 13:59:07 +0100

    refactor: move util functions to sorting_utils.c

--- srcs/sort/sorting_utils.c
+++ srcs/sort/sorting_utils.c
@@ -6,7 +6,7 @@
 /*   By: frodrig2 <frodrig2@students.42porto.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/30 09:02:26 by frodrig2          #+#    #+#             */
-/*   Updated: 2026/08/30 09:24:46 by frodrig2         ###   ########.fr       */
+/*   Updated: 2026/08/30 13:58:04 by frodrig2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,4 +46,36 @@ void	rotate_to_top_a(t_data *data, int index, int size)
 		while (index++ < size)
 			rra(data);
 	}
+}
+
+void	assign_indexes(t_stack *a)
+{
+	t_stack	*i;
+	t_stack	*j;
+	int		rank;
+
+	i = a;
+	while (i)
+	{
+		rank = 0;
+		j = a;
+		while (j)
+		{
+			rank += (j->value < i->value);
+			j = j->next;
+		}
+		i->rank = rank;
+		i = i->next;
+	}
+}
+
+int	is_sorted(t_stack *stack)
+{
+	while (stack && stack->next)
+	{
+		if (stack->value > stack->next->value)
+			return (0);
+		stack = stack->next;
+	}
+	return (1);
 }
